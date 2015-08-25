e9fdd2cbf76d0e2ff1b01f69861dac36068ae86f
spati2 <spati2@ncsu.edu>
2015-08-24 21:03:06 -0400

    Create okok.py

---
+++ hw/code/1/okok.py
@@ -0,0 +1,33 @@
+import time
+from ok import *
+
+print time.strftime("%H:%M:%S\n")
+
+@ok
+def _ok1():
+  assert 1==1
+
+@ok
+def _ok2():
+  assert 2==1
+
+@ok
+def _ok3():
+  assert 3==3 
+
+@ok
+def _ok5():
+    assert True == False
+
+@ok
+def _ok4():
+  assert unittest.tries==4
+  assert unittest.fails==1
+  print unittest.score() 
+
+@ok
+def _ok5():
+   assert unittest.tries==10
+   assert unittest.fails==1
+   print unittest.score()
+    
