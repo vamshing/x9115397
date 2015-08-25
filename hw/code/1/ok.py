b366857f6c66e4a3a160ce0793f2ec2a66e7dc45
spati2 <spati2@ncsu.edu>
2015-08-24 21:03:30 -0400

    Create ok.py

---
+++ hw/code/1/ok.py
@@ -0,0 +1,24 @@
+def ok(*lst):
+  print "### ",lst[0].__name__
+  for one in lst: unittest(one)
+  return one
+
+class unittest:
+  tries = fails = 0  #  tracks the record so far
+  @staticmethod
+  def score():
+    t = unittest.tries
+    f = unittest.fails
+    return "# TRIES= %s FAIL= %s %%PASS = %s%%"  % (
+      t,f,int(round(t*100/(t+f+0.001))))
+  def __init__(i,test):
+    unittest.tries += 1
+    try:
+      test()
+    except Exception,e:
+      unittest.fails += 1
+      i.report(test)
+  def report(i,test):
+    import traceback
+    print traceback.format_exc()
+    print unittest.score(),':',test.__name__
