defbd3ee2ef7a145ab31d2a2cf702c0d4fbdf804
Guduguntla Vamshi <vamshing@gmail.com>
2015-08-30 21:20:38 -0400

    Update think3.py

--- hw/code/2/think3.py
+++ hw/code/2/think3.py
@@ -204,4 +204,118 @@ print_grid()
 
 
 
-print 'helo'
+# # 4.2 - Turtle Flowers
+
+# In[ ]:
+
+import math
+from swampy.TurtleWorld import *
+
+def polyline(t, n, length, angle):
+    """Draws n line segments.
+    t: Turtle object
+    n: number of line segments
+    length: length of each segment
+    angle: degrees between segments
+    """
+    for i in range(n):
+        fd(t, length)
+        lt(t, angle)
+
+
+def arc(t, r, angle):
+    """Draws an arc - This module contains code from Think Python by Allen B. Downey
+    t: Turtle
+    r: radius of arc
+    angle: angle subtended by the arc, in degrees
+    """
+    arc_length = 2 * math.pi * r * abs(angle) / 360
+    n = int(arc_length / 4) + 1
+    step_length = arc_length / n
+    step_angle = float(angle) / n
+    lt(t, step_angle/2)
+    polyline(t, n, step_length, step_angle)
+    rt(t, step_angle/2)
+
+    
+def petal(t,r,angle):
+    for i in range(2):
+        arc(t,r,angle)
+        lt(t,180-angle)
+        
+def flower(t,r,n,angle):
+    for i in range(n):
+        petal(t,r,angle)
+        lt(t,360.0/n)
+    
+def penupmove(t,length):
+    pu(t)
+    fd(t,length)
+    pd(t)
+
+if __name__ == '__main__':
+    world = TurtleWorld()    
+
+    bob = Turtle()
+    bob.delay = 0.001
+    flower(bob,100,7,60)
+    penupmove(bob,200)
+    
+    flower(bob,100,10,60)
+    penupmove(bob,200)
+    
+    flower(bob,250,20,18)
+    penupmove(bob,200)
+  
+    wait_for_user()
+
+
+# # 4.3 Turtle Pies
+
+# In[ ]:
+
+import math
+from swampy.TurtleWorld import *
+"""
+Assuming that the vertices of the polygon fall on the circle,
+there by subtending equal angles at the centre of the circle
+"""
+def triangles(t,n,r,angle):
+    length = 2* r * math.sin(angle * 0.5 * math.pi / 180)
+    for i in range(n):
+        lt(t,(angle*.5))
+        fd(t, r)
+        lt(t, 90+(angle*.5))
+        fd(t, length)
+        lt(t, 90+(angle*.5))
+        fd(t, r)
+        lt(t, 180-(angle*.5))
+
+def penupmove(t,length):
+    pu(t)
+    fd(t,length)
+    pd(t)
+    
+def generator(t,n,r):
+    angle = 360.0/n
+    triangles(bob,n,100,angle)
+    penupmove(t,r+100)   
+
+if __name__ == '__main__':
+    world = TurtleWorld()    
+    bob = Turtle()
+    bob.delay = 0.001
+    
+    #5-gon
+    generator(bob,5,100)
+    #6-gon
+    generator(bob,6,100)
+    #7-gon
+    generator(bob,7,100)
+    
+    wait_for_user()
+
+
+# In[ ]:
+
+
